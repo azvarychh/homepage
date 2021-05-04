45ce8681157a8a5266b46fb0caee1b6ebec5a20d
Andrew <83534081+azvarychh@users.noreply.github.com>
2021-05-04 03:43:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-1
+$(window).scroll(function () {
+	$(".bio-new").each(function () {
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+		if (imagePos < topOfWindow + 650) {
+			$(this).addClass("animate__fadeInLeft");
+		}
+	});
+});
+
+$(function () {
+	$(window).scroll(function () {
+		$(".yt").each(function () {
+			var imagePos = $(this).offset().top;
+
+			var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow + 650) {
+				$(this).addClass("animate__fadeInRight");
+			}
+		});
+	});
+});
